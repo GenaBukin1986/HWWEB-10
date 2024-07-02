4299289fb16ee86a7255753fb4348f24431f96fd
Nick_Bear <bear_76@mail.ru>
2024-07-02 19:25:08 +0300

    Home Work#10 done

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuActive = document.querySelector('.menu-active');
+const burger = document.querySelector('.burger');
+
+function toggleMenu() {
+    menuActive.classList.toggle('hidden');
+}
+
+burger.addEventListener('click', toggleMenu);
